1a3ce5e35415df9aff4e0a0b529c4472fe68e134
Alex R Ribeiro <alexramosribeiro@gmail.com>
2023-11-10 18:55:03 -0300

    Definindo número do computador

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-console.log("Está funcionando...");
-
 const painel1 = document.querySelector("#par-ou-impar");
 const painel2 = document.querySelector("#escolha-numeros");
 const resultado = document.querySelector("#resultado");
+var numeroComputador;
 
 function escolherParOuImpar(parOuImpar) {
     this.parOuImpar = parOuImpar;
@@ -29,4 +28,13 @@ function desabilitarBotoes() {
 function selecionar(id) {
     const botao = document.getElementById(id);
     botao.classList.add('selecionado');
+}
+
+function escolherNumeroComputador() {
+    numeroComputador = Math.floor(Math.random() * 6);
+    console.log(numeroComputador);
+}
+
+function jogarParOuImpar() {
+    console.log('Chamou a função jogar...');
 }
